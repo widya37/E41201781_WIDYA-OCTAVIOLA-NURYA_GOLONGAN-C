44c9a75892350a6366cb36633585b98cf61eede9
widya37 <widyaoctaviolanuriya.1@gmail.com>
2021-10-14 13:08:28 +0700

    tugas selection widget

---
+++ FragmentBottomNavigation/app/src/main/java/com/example/fragmentbuttonnavigation/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.fragmentbuttonnavigation;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigation;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ChatsFragment()).commit();
+
+        bottomNavigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+
+                switch (item.getItemId()) {
+                    case R.id.nav_chats:
+                        selectedFragment = new ChatsFragment();
+                        break;
+                    case R.id.nav_status:
+                        selectedFragment = new StatusFragment();
+                        break;
+                    case R.id.nav_calls:
+                        selectedFragment = new CallsFragment();
+                        break;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+                return true;
+            }
+        });
+    }
+}
