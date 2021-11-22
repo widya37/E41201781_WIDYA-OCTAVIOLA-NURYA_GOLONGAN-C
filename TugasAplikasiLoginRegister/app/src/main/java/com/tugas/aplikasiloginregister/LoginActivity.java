1276212bf48f87529f4d8f5c8423afdc36abf4f1
widya37 <widyaoctaviolanuriya.1@gmail.com>
2021-11-22 18:35:57 +0700

    tugas

---
+++ TugasAplikasiLoginRegister/app/src/main/java/com/tugas/aplikasiloginregister/LoginActivity.java
@@ -0,0 +1,94 @@
+package com.tugas.aplikasiloginregister;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.tugas.aplikasiloginregister.api.ApiClient;
+import com.tugas.aplikasiloginregister.api.ApiInterface;
+import com.tugas.aplikasiloginregister.model.login.Login;
+import com.tugas.aplikasiloginregister.model.login.LoginData;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText etUsername, etPassword;
+    Button btnLogin;
+    String Username, Password;
+    TextView tvRegister;
+    ApiInterface apiInterface;
+    SessionManager sessionManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(this);
+
+        tvRegister = findViewById(R.id.tvCreateAccount);
+        tvRegister.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnLogin:
+                Username = etUsername.getText().toString();
+                Password = etPassword.getText().toString();
+                login(Username, Password);
+                break;
+            case R.id.tvCreateAccount:
+                Intent intent = new Intent(this,RegisterActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
+
+    private void login(String username, String password) {
+
+        apiInterface = ApiClient.getClient().create(ApiInterface.class);
+        Call<Login> loginCall = apiInterface.loginResponse(username, password);
+        loginCall.enqueue(new Callback<Login>() {
+            @Override
+            public void onResponse(Call<Login> call, Response<Login> response) {
+                if(response.body() != null && response.isSuccessful() && response.body().isStatus()){
+
+                    //untuk menyimpan sesi
+                    sessionManager = new SessionManager(LoginActivity.this);
+                    LoginData loginData = response.body().getLoginData();
+                    sessionManager.createLoginSession(loginData);
+
+                    //untuk pindah
+                    Toast.makeText(LoginActivity.this, response.body().getLoginData().getName(), Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Toast.makeText(LoginActivity.this, response.body().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<Login> call, Throwable t) {
+                Toast.makeText(LoginActivity.this, t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+}
---
+++ TugasAplikasiLoginRegister/app/src/main/java/com/tugas/aplikasiloginregister/api/ApiInterface.java
@@ -0,0 +1,28 @@
+package com.tugas.aplikasiloginregister.api;
+
+import com.tugas.aplikasiloginregister.model.login.Login;
+import com.tugas.aplikasiloginregister.model.register.Register;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface ApiInterface {
+
+    @FormUrlEncoded
+    @POST("login.php")
+    Call<Login> loginResponse(
+            @Field("username") String username,
+            @Field("password") String password
+    );
+
+    @FormUrlEncoded
+    @POST("register.php")
+    Call<Register> registerResponse(
+            @Field("username") String username,
+            @Field("password") String password,
+            @Field("name") String name
+    );
+
+}
